304c37a670eaa99e2490227c6b1a0a1765ca1a4d
Resendii <gabriel@sensio.com.br>
2023-06-21 20:35:30 -0300

    feat(resolvers): set appointment resolver

---
+++ src/resolvers/appointments-resolver.ts
@@ -0,0 +1,38 @@
+import { Arg, Resolver, Query, Mutation, FieldResolver, Root } from "type-graphql";
+import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input";
+import { Appointment } from "../dtos/models/appointment-model";
+import { Costumer } from "../dtos/models/costumer-model";
+
+
+@Resolver(() => Appointment)
+export class AppointmentResolvers {
+
+    @Query(() => [Appointment])
+    async appointments() {
+        return [
+            {
+                startsAt: new Date(),
+                endsAt: new Date()
+            }
+        ];
+    }
+
+    @Mutation(() => Appointment)
+    async createAppointment(@Arg('data') data: CreateAppointmentInput) {
+        const appointment = {
+            startsAt: data.startsAt,
+            endsAt: data.endsAt
+        }
+
+        return appointment;
+    }
+
+    @FieldResolver(() => Costumer)
+    async costumer(@Root() appointment: Appointment) {
+        console.log(appointment);
+
+        return {
+            name: 'John Doe'
+        }
+    }
+}
